d550825fc44d8c3108f5dbcce4a3947d89111fd5
Garcia Canul Guillermo de Jesus <22393204@utcancun.edu.mx>
2023-11-18 22:50:19 -0500

    Cambio comida

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,6 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import morgan from 'morgan';
 import multer from 'multer';
-import paypal from 'paypal-rest-sdk';
-
-paypal.configure({
-  mode: 'sandbox', // Set this to 'live' for production environment
-  client_id: 'AU9szlwCmWWHB8Pk4lmuQkyArUn9FQkr8GkUTQ4K5saTz9yoJxeUjIPASeDZOG5vWyoz4lW7y5Paempq',
-  client_secret: 'EBDDabpWfnBNNj3PlqfmNS_L64c9MTpH2YChYjvlSbV58VRGGmrcNdtdXiIp_rhyoDeJa3c11CRQCejz',
-});
 
 const app = express();
 app.use(express.json());
@@ -22,7 +15,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'ecommerce',
+  database: 'gym',
 });
 
 connection.connect((error) => {
@@ -32,64 +25,6 @@ connection.connect((error) => {
     console.log('Conexión con el servidor exitosa');
   }
 });
-
-// Route to initiate PayPal payment
-app.post('/createPayment', (req, res) => {
-  const { totalAmount, currency, description } = req.body;
-
-  const paymentData = {
-    intent: 'sale',
-    payer: {
-      payment_method: 'paypal',
-    },
-    redirect_urls: {
-      return_url: 'http://localhost:3000/Finalizacion', // Replace this with your success URL
-      cancel_url: 'http://localhost:8081/cancel', // Replace this with your cancel URL
-    },
-    transactions: [{
-      amount: {
-        total: totalAmount,
-        currency,
-      },
-      description,
-    }],
-  };
-
-  // Create the payment
-  paypal.payment.create(paymentData, (error, payment) => {
-    if (error) {
-      console.error('Error creating PayPal payment:', error);
-      res.status(500).json({ error: 'Error creating PayPal payment' });
-    } else {
-      // Redirect the user to the PayPal payment approval URL
-      const approvalURL = payment.links.find(link => link.rel === 'approval_url').href;
-      res.json({ approvalURL });
-    }
-  });
-});
-
-// Route for PayPal payment success
-app.get('/success', (req, res) => {
-  const { paymentId, PayerID } = req.query;
-
-  // Execute the payment
-  paypal.payment.execute(paymentId, { payer_id: PayerID }, (error, payment) => {
-    if (error) {
-      console.error('Error executing PayPal payment:', error);
-      res.status(500).json({ error: 'Error executing PayPal payment' });
-    } else {
-      // Payment successfully executed, do something with the payment details if needed
-      res.json({ message: 'Payment successful' });
-    }
-  });
-});
-
-// Route for PayPal payment cancel
-app.get('/cancel', (req, res) => {
-  res.json({ message: 'Payment canceled' });
-});
-
-
 
 const storage = multer.diskStorage({
   destination: '../frontend/src/images/Productos', // Change the destination folder to your desired location
@@ -118,143 +53,6 @@ app.post('/addproducto', upload.single('image'), (req, res) => {
     }
 
     res.json({ message: 'Producto añadido correctamente' });
-  });
-});
-
-
-
-
-// Ruta para obtener todos los productos
-app.get('/productos', (req, res) => {
-  const query = 'SELECT * FROM productos';
-
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error al obtener los productos:', err);
-      res.status(500).json({ error: 'Error al obtener los productos' });
-      return;
-    }
-
-    res.json(results);
-  });
-});
-
-// Ruta para obtener todas las categorías
-app.get('/categorias', (req, res) => {
-  const query = 'SELECT * FROM categorias';
-
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error al obtener las categorías:', err);
-      res.status(500).json({ error: 'Error al obtener las categorías' });
-      return;
-    }
-
-    res.json(results);
-  });
-});
-
-// Ruta para obtener los productos por categoría
-app.get('/categorias/:id', (req, res) => {
-  const id = req.params.id;
-  const query = `SELECT * FROM productos WHERE categoria_id = ?`;
-
-  connection.query(query, [id], (err, results) => {
-    if (err) {
-      console.error('Error al obtener los productos por categoría:', err);
-      res.status(500).json({ error: 'Error al obtener los productos por categoría' });
-      return;
-    }
-
-    res.json(results);
-  });
-});
-
-// Ruta para añadir una nueva categoría
-app.post('/addcategorias', (req, res) => {
-  const { nombre } = req.body;
-  const query = `INSERT INTO categorias (nombre) VALUES (?)`;
-  const values = [nombre];
-
-  connection.query(query, values, (err, results) => {
-    if (err) {
-      console.error('Error al añadir la categoría:', err);
-      res.status(500).json({ error: 'Error al añadir la categoría' });
-      return;
-    }
-
-    res.json({ message: 'Categoría añadida correctamente' });
-  });
-});
-
-
-
-//RUTA PARA ELIMINAR UN PRODUCTO
-app.delete('/delproductos/:id', (req, res) => {
-  const id = req.params.id;
-  const query = `DELETE FROM productos WHERE id = ?`;
-
-  connection.query(query, [id], (err, results) => {
-    if (err) {
-      console.error('Error al eliminar el producto:', err);
-      res.status(500).json({ error: 'Error al eliminar el producto' });
-      return;
-    }
-
-    res.json({ message: 'Producto eliminado correctamente' });
-  });
-});
-
-// Ruta para modificar un producto
-app.put('/editproductos/:id', (req, res) => {
-  const id = req.params.id;
-  const { nombre, precio } = req.body;
-  const query = `UPDATE productos SET nombre = ?, precio = ? WHERE id = ?`;
-  const values = [nombre, precio, id];
-
-  connection.query(query, values, (err, results) => {
-    if (err) {
-      console.error('Error al modificar el producto:', err);
-      res.status(500).json({ error: 'Error al modificar el producto' });
-      return;
-    }
-
-    res.json({ message: 'Producto modificado correctamente' });
-  });
-});
-
-
-//RUTA PARA ELIMINAR UNA CATEGORIA 
-app.delete('/delcategoria/:id', (req, res) => {
-  const id = req.params.id;
-  const query = `DELETE FROM categorias WHERE id = ?`;
-
-  connection.query(query, [id], (err, results) => {
-    if (err) {
-      console.error('Error al eliminar la categoría:', err);
-      res.status(500).json({ error: 'Error al eliminar la categoría' });
-      return;
-    }
-
-    res.json({ message: 'Categoría eliminada correctamente' });
-  });
-});
-
-// Ruta para modificar el nombre de una categoría
-app.put('/editcategoria/:id', (req, res) => {
-  const id = req.params.id;
-  const { nombre } = req.body;
-  const query = `UPDATE categorias SET nombre = ? WHERE id = ?`;
-  const values = [nombre, id];
-
-  connection.query(query, values, (err, results) => {
-    if (err) {
-      console.error('Error al modificar el nombre de la categoría:', err);
-      res.status(500).json({ error: 'Error al modificar el nombre de la categoría' });
-      return;
-    }
-
-    res.json({ message: 'Nombre de la categoría modificado correctamente' });
   });
 });
 
@@ -376,23 +174,27 @@ app.post('/registrar', (req, res) => {
   });
 });
 
-  //* LOGIN
-  app.post("/login", (peticion, respuesta) => {
-    const { email, contrasena } = peticion.body;
-    const query = "SELECT contrasena FROM usuarios WHERE email = ?";
-    connection.query(query, [email], (error, resultados) => {
-      if (error) return respuesta.json({ Error: "Error en la consulta." });
-      if (resultados.length === 0) return respuesta.json({ Error: "Error en la consulta" });
-      const usuario = resultados[0];
-      const match = bcrypt.compareSync(contrasena, usuario.contrasena);
-      if (match) {
-        const token = jwt.sign({ email: email }, "secreto"); // Corrige el campo aquí
-        return respuesta.json({ Estatus: "CORRECTO", Resultado: usuario, token });
-      } else {
-        return respuesta.json({ Error: "Error en las credenciales del usuario" });
-      }
-    });
+app.post("/login", (peticion, respuesta) => {
+  const { email, contrasena } = peticion.body;
+
+  // Cambia la consulta para obtener también el ID del usuario
+  const query = "SELECT id, contrasena FROM usuarios WHERE email = ?";
+  connection.query(query, [email], (error, resultados) => {
+    if (error) return respuesta.json({ Error: "Error en la consulta." });
+    if (resultados.length === 0) return respuesta.json({ Error: "Usuario no encontrado." });
+
+    const usuario = resultados[0];
+    const match = bcrypt.compareSync(contrasena, usuario.contrasena);
+
+    if (match) {
+      // Incluye tanto el email como el id en el token
+      const token = jwt.sign({ email: usuario.email, id: usuario.id }, "secreto"); // Asegúrate de que 'secreto' sea una clave segura en producción
+      return respuesta.json({ Estatus: "CORRECTO", Resultado: usuario, token });
+    } else {
+      return respuesta.json({ Error: "Error en las credenciales del usuario" });
+    }
   });
+});
   
   app.post("/VerificarCorreo", (peticion, respuesta) => {
     const { email } = peticion.body;
@@ -442,153 +244,37 @@ app.post('/registrar', (req, res) => {
     });
   });
 
+//Quitar favoritos
 
-// Ruta para agregar un nuevo pedido
-app.post('/nuevopedido', (req, res) => {
-  const { usuario_id, total, detalles } = req.body;
+app.delete('/favoritos', (req, res) => {
+  const { usuario_id, platillo_id } = req.body;
+  const query = 'DELETE FROM favoritos_usuario WHERE usuario_id = ? AND platillo_id = ?';
 
-  // Primero, insertamos el pedido en la tabla "pedidos"
-  const insertPedidoQuery = `INSERT INTO pedidos (usuario_id, fecha_pedido, estado, total) VALUES (?, NOW(), 'Correcto', ?)`;
-  connection.query(insertPedidoQuery, [usuario_id, total], (err, resultPedido) => {
+  connection.query(query, [usuario_id, platillo_id], (err, results) => {
     if (err) {
-      console.error('Error al insertar el pedido:', err);
-      res.status(500).json({ error: 'Error al insertar el pedido' });
+      console.error('Error al quitar de favoritos:', err);
+      res.status(500).json({ error: 'Error al quitar de favoritos' });
       return;
     }
 
-    const pedido_id = resultPedido.insertId; // Obtener el ID del pedido recién insertado
-
-    // Luego, insertamos los detalles del pedido en la tabla "detalles_pedidos"
-    const insertDetallesQuery = `INSERT INTO detalles_pedidos (pedido_id, producto_id, cantidad, precio_unitario) VALUES ?`;
-    const detallesValues = detalles.map(({ producto_id, cantidad, precio_unitario }) => [pedido_id, producto_id, cantidad, precio_unitario]);
-
-    connection.query(insertDetallesQuery, [detallesValues], (err, resultDetalles) => {
-      if (err) {
-        console.error('Error al insertar los detalles del pedido:', err);
-        res.status(500).json({ error: 'Error al insertar los detalles del pedido' });
-        return;
-      }
-
-      res.json({ message: 'Pedido insertado correctamente' });
-    });
+    res.json({ message: 'Eliminado de favoritos correctamente' });
   });
 });
 
+//Obtener Favoritos
 
+app.get('/favoritos/:usuario_id', (req, res) => {
+  const usuario_id = req.params.usuario_id;
+  const query = 'SELECT * FROM favoritos_usuario WHERE usuario_id = ?';
 
-  // Ruta para obtener todos los pedidos
-app.get('/pedidos', (req, res) => {
-  const query = 'SELECT * FROM pedidos';
-
-  connection.query(query, (err, results) => {
+  connection.query(query, [usuario_id], (err, results) => {
     if (err) {
-      console.error('Error al obtener los pedidos:', err);
-      res.status(500).json({ error: 'Error al obtener los pedidos' });
+      console.error('Error al obtener favoritos:', err);
+      res.status(500).json({ error: 'Error al obtener favoritos' });
       return;
     }
 
     res.json(results);
-  });
-});
-
-
-
-
-
-app.delete('/delpedido/:id', (req, res) => {
-  const id = req.params.id;
-
-  // Primero, eliminamos los detalles de pedido asociados al pedido
-  const deleteDetallesQuery = `DELETE FROM detalles_pedidos WHERE pedido_id = ?`;
-  connection.query(deleteDetallesQuery, [id], (err, resultDetalles) => {
-    if (err) {
-      console.error('Error al eliminar los detalles del pedido:', err);
-      res.status(500).json({ error: 'Error al eliminar los detalles del pedido' });
-      return;
-    }
-
-    // Luego, eliminamos el pedido
-    const deletePedidoQuery = `DELETE FROM pedidos WHERE id = ?`;
-    connection.query(deletePedidoQuery, [id], (err, resultPedido) => {
-      if (err) {
-        console.error('Error al eliminar el pedido:', err);
-        res.status(500).json({ error: 'Error al eliminar el pedido' });
-        return;
-      }
-
-      res.json({ message: 'Pedido eliminado correctamente' });
-    });
-  });
-});
-
-
-
-// Ruta para obtener un pedido y sus detalles de productos por su ID
-app.get('/detalle-pedido/:id', (req, res) => {
-  const pedidoId = req.params.id;
-
-  // Consultar el pedido y sus detalles de productos
-  const query = `
-    SELECT 
-      p.id AS pedido_id,
-      p.fecha_pedido,
-      p.estado,
-      dp.id AS detalle_id,
-      dp.cantidad,
-      pr.id AS producto_id,
-      pr.nombre AS producto_nombre,
-      pr.descripcion AS producto_descripcion,
-      pr.precio AS producto_precio,
-      pr.stock AS producto_stock,
-      pr.image AS producto_image
-    FROM 
-      pedidos p
-    JOIN 
-      detalles_pedidos dp ON p.id = dp.pedido_id
-    JOIN 
-      productos pr ON dp.producto_id = pr.id
-    WHERE p.id = ?;
-  `;
-
-  connection.query(query, [pedidoId], (err, results) => {
-    if (err) {
-      console.error('Error al obtener el pedido y sus detalles de productos:', err);
-      res.status(500).json({ error: 'Error al obtener el pedido y sus detalles de productos' });
-      return;
-    }
-
-    if (results.length === 0) {
-      res.status(404).json({ error: 'Pedido no encontrado' });
-      return;
-    }
-
-    // Organizar los datos en una estructura más legible
-    const pedidoInfo = {
-      pedido_id: results[0].pedido_id,
-      fecha_pedido: results[0].fecha_pedido,
-      estado: results[0].estado,
-      detalles: []
-    };
-
-    results.forEach(row => {
-      const detalleProducto = {
-        detalle_id: row.detalle_id,
-        cantidad: row.cantidad,
-        precio_unitario: row.producto_precio, // Renombrar precio a precio_unitario
-        producto: {
-          producto_id: row.producto_id,
-          nombre: row.producto_nombre,
-          descripcion: row.producto_descripcion,
-          precio: row.producto_precio,
-          stock: row.producto_stock,
-          image: row.producto_image
-        }
-      };
-
-      pedidoInfo.detalles.push(detalleProducto);
-    });
-
-    res.json(pedidoInfo);
   });
 });
 
--- frontend/src/Views/Comida.jsx
+++ frontend/src/Views/Comida.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import jwt_decode from 'jwt-decode'; // Importa jwt-decode
 import {
-  RiMenu3Fill,
-  RiUser3Line,
-  RiAddLine,
-  RiPieChartLine,
-  RiCloseLine,
-  RiArrowDownSLine,
+    RiMenu3Fill,
+    RiUser3Line,
+    RiAddLine,
+    RiPieChartLine,
+    RiCloseLine,
+    RiArrowDownSLine,
 } from 'react-icons/ri';
 // Components
 import Sidebar from '../Components/Sidebar';
@@ -13,83 +15,129 @@ import Header from '../Components/Header';
 import Card from '../Components/Card';
 
 function Comida() {
-  const [showMenu, setShowMenu] = useState(false);
-  const [showOrder, setShowOrder] = useState(false);
-  const [favorites, setFavorites] = useState(new Set()); // Estado para controlar los favoritos
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-    setShowOrder(false);
-  };
+    const [showMenu, setShowMenu] = useState(false);
+    const [showOrder, setShowOrder] = useState(false);
+    const [favorites, setFavorites] = useState(new Set()); // Estado para controlar los favoritos
 
-  const toggleOrders = () => {
-    setShowOrder(!showOrder);
-    setShowMenu(false);
-  };
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+        setShowOrder(false);
+      };
+    
+      const toggleOrders = () => {
+        setShowOrder(!showOrder);
+        setShowMenu(false);
+      };
 
-  // Función para añadir o quitar un plato de los favoritos
-  const toggleFavorite = (dishId) => {
-    setFavorites((prevFavorites) => {
-      const newFavorites = new Set(prevFavorites);
-      if (newFavorites.has(dishId)) {
-        newFavorites.delete(dishId);
-      } else {
-        newFavorites.add(dishId);
-      }
-      return newFavorites;
-    });
-  };
+    // Decodifica el token para obtener el ID del usuario
+    const token = localStorage.getItem('token'); // Asegúrate de que el token se almacene aquí después del login
+    const decodedToken = token ? jwt_decode(token) : null;
+    const usuario_id = decodedToken ? decodedToken.id : null;
 
-  // Imagina que esta es la lista de platos que podrías tener
-  const dishes = [
-    { id: 1, name: 'Spicy seasoned seafood noodles', img: '../../public/comida.png', price: '2.29', inventory: '20' },
-    { id: 2, name: 'Chicken curry bowl', img: '../../public/dish.png', price: '3.99', inventory: '15' },
-    // ... otros platos ...
-  ];
+    // Lista de platillos (debería venir del backend en una aplicación real)
+    const dishes = [
+        { id: 1, nombre: 'Platillo 1', img: 'ruta-imagen-1.jpg', descriptionPlatillo: 'Descripción del Platillo 1', categoria: 'Niños' },
+        { id: 2, nombre: 'Platillo 2', img: 'ruta-imagen-2.jpg', descriptionPlatillo: 'Descripción del Platillo 2', categoria: 'Adultos' },
+        // ... otros platillos ...
+    ];
 
-  return (
-    <div className="bg-[#262837] w-full min-h-screen h-full">
-      <Sidebar showMenu={showMenu} />
-      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
-        <button className="p-2">
-          <RiUser3Line />
-        </button>
-        <button className="p-2">
-          <RiAddLine />
-        </button>
-        <button onClick={toggleOrders} className="p-2">
-          <RiPieChartLine />
-        </button>
-        <button onClick={toggleMenu} className="text-white p-2">
-          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
-        </button>
-      </nav>
-      <main className="lg:pl-32 pb-20">
-        <div className="md:p-8 p-4">
-          <Header />
-          <div className="flex items-center justify-between mb-16">
-            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
-              <RiArrowDownSLine /> Dine in
-            </button>
-          </div>
-          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16">
-            {dishes.map((dish) => (
-              <Card
-                key={dish.id}
-                img={dish.img}
-                description={dish.name}
-                price={dish.price}
-                inventory={dish.inventory}
-                isFavorite={favorites.has(dish.id)}
-                onToggleFavorite={() => toggleFavorite(dish.id)}
-              />
-            ))}
-          </div>
+    const toggleFavorite = async (dishId) => {
+        const isFavorite = favorites.has(dishId);
+        const method = isFavorite ? 'DELETE' : 'POST';
+        const url = '/api/favoritos'; // URL para agregar/quitar favoritos
+
+        try {
+            const response = await fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Aquí incluirías cualquier header de autenticación si es necesario
+                },
+                body: JSON.stringify({ usuario_id, platillo_id: dishId })
+            });
+
+            if (response.ok) {
+                setFavorites((prevFavorites) => {
+                    const newFavorites = new Set(prevFavorites);
+                    if (isFavorite) {
+                        newFavorites.delete(dishId);
+                    } else {
+                        newFavorites.add(dishId);
+                    }
+                    return newFavorites;
+                });
+            } else {
+                // Manejo de errores
+                console.error('Error al actualizar favoritos');
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+        }
+    };
+
+    useEffect(() => {
+        const fetchFavorites = async () => {
+          if (usuario_id) {
+            try {
+              const response = await fetch(`/api/favoritos/${usuario_id}`);
+              if (response.ok) {
+                const data = await response.json();
+                setFavorites(new Set(data.map(item => item.platillo_id)));
+              } else {
+                console.error('Error al cargar los favoritos');
+              }
+            } catch (error) {
+              console.error('Error en la solicitud:', error);
+            }
+          }
+        };
+      
+        fetchFavorites();
+      }, [usuario_id]); // Dependencia en usuario_id para recargar cuando cambie
+
+    return (
+        <div className="bg-[#262837] w-full min-h-screen h-full">
+            <Sidebar showMenu={showMenu} />
+            <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+                <button className="p-2">
+                    <RiUser3Line />
+                </button>
+                <button className="p-2">
+                    <RiAddLine />
+                </button>
+                <button onClick={toggleOrders} className="p-2">
+                    <RiPieChartLine />
+                </button>
+                <button onClick={toggleMenu} className="text-white p-2">
+                    {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+                </button>
+            </nav>
+            <main className="lg:pl-32 pb-20">
+                <div className="md:p-8 p-4">
+                    <Header />
+                    <div className="flex items-center justify-between mb-16">
+                        <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+                        <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+                            <RiArrowDownSLine /> Dine in
+                        </button>
+                    </div>
+                    <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16">
+                        {dishes.map((dish) => (
+                            <Card
+                                key={dish.id}
+                                img={dish.img}
+                                description={dish.nombre}
+                                detail={dish.descriptionPlatillo}
+                                isFavorite={favorites.has(dish.id)}
+                                onToggleFavorite={() => toggleFavorite(dish.id)}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
 
 export default Comida;

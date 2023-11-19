d21e01c2afb7a8526229ae03a5af38aeaa6dccc9
Garcia Canul Guillermo de Jesus <22393204@utcancun.edu.mx>
2023-11-18 19:55:10 -0500

    Agregar Favoritos

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,19 +1,27 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { RiHeart3Line, RiHeartFill } from 'react-icons/ri'; // Importamos ambos íconos para los estados de favorito/no favorito
 
-const Card = (props) => {
-  const { img, description, price, inventory } = props;
-
+const Card = ({ img, description, price, inventory, isFavorite, onToggleFavorite }) => {
   return (
     <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
       <img
         src={img}
+        alt="Dish"
         className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
       />
       <p className="text-xl">{description}</p>
       <span className="text-gray-400">${price}</span>
       <p className="text-gray-600">{inventory} Bowls available</p>
+      {/* Botón de favorito que cambia de ícono basado en si es favorito o no */}
+      <button onClick={onToggleFavorite} className="text-3xl">
+        {isFavorite ? (
+          <RiHeartFill className="text-red-500" /> // Ícono lleno si es favorito
+        ) : (
+          <RiHeart3Line className="text-red-500" /> // Ícono de borde si no es favorito
+        )}
+      </button>
     </div>
   );
 };
--- frontend/src/Views/Comida.jsx
+++ frontend/src/Views/Comida.jsx
@@ -15,6 +15,7 @@ import Card from '../Components/Card';
 function Comida() {
   const [showMenu, setShowMenu] = useState(false);
   const [showOrder, setShowOrder] = useState(false);
+  const [favorites, setFavorites] = useState(new Set()); // Estado para controlar los favoritos
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -26,10 +27,29 @@ function Comida() {
     setShowMenu(false);
   };
 
+  // Función para añadir o quitar un plato de los favoritos
+  const toggleFavorite = (dishId) => {
+    setFavorites((prevFavorites) => {
+      const newFavorites = new Set(prevFavorites);
+      if (newFavorites.has(dishId)) {
+        newFavorites.delete(dishId);
+      } else {
+        newFavorites.add(dishId);
+      }
+      return newFavorites;
+    });
+  };
+
+  // Imagina que esta es la lista de platos que podrías tener
+  const dishes = [
+    { id: 1, name: 'Spicy seasoned seafood noodles', img: '../../public/comida.png', price: '2.29', inventory: '20' },
+    { id: 2, name: 'Chicken curry bowl', img: '../../public/dish.png', price: '3.99', inventory: '15' },
+    // ... otros platos ...
+  ];
+
   return (
     <div className="bg-[#262837] w-full min-h-screen h-full">
       <Sidebar showMenu={showMenu} />
-      {/* Mobile Menu */}
       <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
         <button className="p-2">
           <RiUser3Line />
@@ -46,50 +66,26 @@ function Comida() {
       </nav>
       <main className="lg:pl-32 pb-20">
         <div className="md:p-8 p-4">
-          {/* Header */}
           <Header />
-          {/* Title and Dine-in Button */}
           <div className="flex items-center justify-between mb-16">
             <h2 className="text-xl text-gray-300">Choose Dishes</h2>
             <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
               <RiArrowDownSLine /> Dine in
             </button>
           </div>
-          {/* Cards Container */}
           <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16">
-            {/* Card Instances */}
-            <Card
-              img="comida.png"
-              description="Spicy seasoned seafood noodles"
-              price="2.29"
-              inventory="20"
-            />
-            {/* Repeat for each dish */}
-            <Card
-              img="dish.png"
-              description="Spicy seasoned seafood noodles"
-              price="2.29"
-              inventory="20"
-            />
-            <Card
-              img="comida.png"
-              description="Spicy seasoned seafood noodles"
-              price="2.29"
-              inventory="20"
-            />
-            <Card
-              img="dish.png"
-              description="Spicy seasoned seafood noodles"
-              price="2.29"
-              inventory="20"
-            />
-            <Card
-              img="comida.png"
-              description="Spicy seasoned seafood noodles"
-              price="2.29"
-              inventory="20"
-            />
-            </div>
+            {dishes.map((dish) => (
+              <Card
+                key={dish.id}
+                img={dish.img}
+                description={dish.name}
+                price={dish.price}
+                inventory={dish.inventory}
+                isFavorite={favorites.has(dish.id)}
+                onToggleFavorite={() => toggleFavorite(dish.id)}
+              />
+            ))}
+          </div>
         </div>
       </main>
     </div>
